868ed83fce9d551f7fcbb29ad40d7805816adecf
MaAs0706e <aswanthmadhav07@gmail.com>
2026-06-17 22:30:26 +0530

    just checking

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -5,6 +5,7 @@ import Hero from "../components/landing/Hero";
 export default function LandingPage() {
   return (
     <div className="min-h-screen bg-[#0B0B0B] text-white">
+        
       <Navbar />
       <Hero />
     </div>
